68579dbf6074561378acb805c448937d5ec75156
sallaball <sallaball1206@gmail.com>
2022-06-13 11:38:05 -0500

    made the input fields work

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,15 @@ var createTaskHandler = function(event) {
     event.preventDefault();
     
     var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
     var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = taskNameInput;
+        listItemEl.className = "task-item"; 
+
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
     tasksToDoEl.appendChild(listItemEl);
     
 };
