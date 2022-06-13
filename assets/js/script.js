6cc275d3aeaae49bd888aa323ea420598a6627d0
sallaball <sallaball1206@gmail.com>
2022-06-13 12:24:39 -0500

    made site more user friendly

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,15 @@ var createFormHandler = function(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+     if (!taskNameInput || !taskTypeInput) {
+         alert("You need to fill out the task form!");
+         return false;
+    }
+
+    formEl.reset();
+
+    formEl.reset();
     
     var taskDataObj = {
         name: taskNameInput,
